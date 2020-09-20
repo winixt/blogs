79812ec75052469ec6923e6a26b7ff976dd1a802
bac-joker <haizekuo@gmail.com>
2020-09-20 16:58:37 +0800

    perf: 提升构建性能

--- .github/actions/generate-changelog/main.js
+++ .github/actions/generate-changelog/main.js
@@ -3,7 +3,7 @@ const cp = require("child_process");
 cp.execSync(`cd ${__dirname}; npm ci`);
 
 const path = require("path");
-// const core = require("@actions/core");
+const core = require("@actions/core");
 const lernaChangelog = path.resolve(__dirname, "node_modules/.bin/conventional-changelog");
 
 const exec = cmd => cp.execSync(cmd).toString().trim();
@@ -11,4 +11,4 @@ const exec = cmd => cp.execSync(cmd).toString().trim();
 const changelog = exec(`node ${lernaChangelog}  -p cmyr-config -r 2`);
 console.log(changelog);
 
-// core.setOutput("changelog", JSON.stringify(changelog.split('\n').slice(4).join('\n')));
+core.setOutput("changelog", JSON.stringify(changelog.split('\n').slice(4).join('\n')));
